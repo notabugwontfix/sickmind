a0a19879311955e990a9272f8e52f149d84cef7a
Not-a-Bug Won't Fix <notabugwontfix@proton.me>
2022-07-06 00:43:10 +0300

    Add debug::Stopwatch

---
+++ src/debugutils/src/Stopwatch.cpp
@@ -0,0 +1,19 @@
+#include <debug/Stopwatch.h>
+#include <debug/Trace.h>
+
+namespace debug {
+
+Stopwatch::Stopwatch(std::string name)
+    : m_name{std::move(name)}, m_start{std::chrono::steady_clock::now()} {}
+
+Stopwatch::~Stopwatch() {
+  const auto now = std::chrono::steady_clock::now();
+
+  trace(TraceInfo) << "Timer " << m_name << ": "
+                   << std::chrono::duration_cast<std::chrono::microseconds>(
+                          now - m_start)
+                          .count()
+                   << " us";
+}
+
+} // namespace debug
